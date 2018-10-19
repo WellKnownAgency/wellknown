51d0ed558744da82afcda8e8a9112a2e077bf4f0
anton <anton.likhnekevich@gmail.com>
2018-10-19 14:50:11 -0400

    fixes

---
+++ resources/views/admin/partials/_js.blade.php
@@ -0,0 +1,15 @@
+<script src="{!! asset('js/app.js') !!}"></script>
+<!-- Core plugin JavaScript-->
+<script src="vendor/admin/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Page level plugin JavaScript-->
+<script src="/vendor/admin/chart.js/Chart.min.js"></script>
+<script src="/vendor/admin/datatables/jquery.dataTables.js"></script>
+<script src="/vendor/admin/datatables/dataTables.bootstrap4.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="js/sb-admin.min.js"></script>
+
+<!-- Demo scripts for this page-->
+<script src="js/demo/datatables-demo.js"></script>
+<script src="js/demo/chart-area-demo.js"></script>
