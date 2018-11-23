d675c8cebc5024f61e7200bc0e2a691ee3ee4c1d
Taras <taraskim@wknown.com>
2018-11-23 13:26:41 -0500

    initial commit

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -133,7 +133,7 @@ class PagesController extends Controller
   public function getSingle($slug) {
     $category = Category::get();
     $post = Post::where('slug', '=', $slug)->first();
-    $posts = Post::latest()->where('id', '!=', $post->id)->where('category_id', '=', $category->id)->limit(3)->get();
+    $posts = Post::latest()->where('id', '!=', $post->id)->where('category_id', '==', $category->id)->limit(3)->get();
     return view('blog.single')->withPost($post)->withPosts($posts)->withCategory($category);
   }
 
