9acd10c55ed0bce876c58a6f5fe8ff5ce34dfe4b
Taras <taraskim@wknown.com>
2018-12-10 19:10:25 -0500

    initial commit

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,6 +4,27 @@
 	@include('partials/_head')
 	</head>
 	<body class="index-page">
+		<script>
+		  window.fbAsyncInit = function() {
+		    FB.init({
+		      appId            : '131016570928938',
+		      autoLogAppEvents : true,
+		      xfbml            : true,
+		      version          : 'v2.11'
+		    });
+		  };
+
+		  (function(d, s, id){
+		     var js, fjs = d.getElementsByTagName(s)[0];
+		     if (d.getElementById(id)) {return;}
+		     js = d.createElement(s); js.id = id;
+		     js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+		     fjs.parentNode.insertBefore(js, fjs);
+		   }(document, 'script', 'facebook-jssdk'));
+		</script>
+		<div class="fb-customerchat"
+		 page_id="<153425045259276>">
+		</div>
 		@include('partials/_nav')
 		<!-- Page Content -->
 		<div id="app" class="presentation-page">
