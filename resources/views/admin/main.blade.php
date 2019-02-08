e4752ccde05aa1d44d7bccaebb8e7d02e70d6d64
Taras <taraskim@wknown.com>
2019-02-08 10:57:52 -0500

    fixes

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -6,6 +6,20 @@
 	      'csrfToken' => csrf_token(),
 	    ]) !!};
 	  </script>
+		<link rel="manifest" href="/manifest.json" />
+		<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+		<script>
+		  var OneSignal = window.OneSignal || [];
+		  OneSignal.push(function() {
+		    OneSignal.init({
+		      appId: "7703c41f-8219-4532-9184-e78dae609e6b",
+		      autoRegister: true,
+		      notifyButton: {
+		        enable: false,
+		      },
+		    });
+		  });
+		</script>
   </head>
   <body id="page-top" class="sidebar-toggled">
     <div id="app">
