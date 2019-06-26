bfb7821467db09be84e5342132788d36d629d45e
Taras <taraskim@wknown.com>
2019-06-25 23:56:58 -0400

    AMP fixes

--- resources/views/partials/_footeramp.blade.php
+++ resources/views/partials/_footeramp.blade.php
@@ -1,3 +1,5 @@
 <footer style="text-align:center;">
-
+	<div >
+		Follow us
+	</div>
 </footer>
--- resources/views/partials/_navamp.blade.php
+++ resources/views/partials/_navamp.blade.php
@@ -1,3 +1,3 @@
-<div style="height:80px; -webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.15); box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.15); background: rgba(220, 55, 65, 0.6) !important;">
+<div style="height:80px; box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.15); background: rgba(220, 55, 65, 0.6);">
 	<amp-img src="/images/wk.jpg" alt="WellKnown Agency Logo" width="100" height="47" style="border:0; margin:20px 16px 10px 16px;"></amp-img>
 </div>
