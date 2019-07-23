8dd5c007501c3e3fa1d1cbd77fdda95d91cb860e
Taras <taraskim@wknown.com>
2019-07-23 00:45:02 -0400

    fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,8 +37,6 @@ class HomeController extends Controller
 
 				$review->headers->set();
 
-			return response($review)->header('Content-type: application/json')
-															->header('Access-Control-Allow-Credentials: true')
-															->header('Access-Control-Allow-Origin: *.ampproject.org');
+			return response($review)->header('Content-type: application/json');
     }
 }
