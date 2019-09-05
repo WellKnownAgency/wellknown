e60d058ce5f6fa3502de4474fcbbb28c64c3a1ca
Taras Kim <kimtaras@gmail.com>
2019-09-05 03:00:19 -0400

    ad deleted

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -65,10 +65,4 @@
 @yield('extrahead')
 <script type="text/javascript" src="https://widget.clutch.co/static/js/widget.js"></script>
 <script async src="https://cdn.ampproject.org/v0.js"></script>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({
-          google_ad_client: "ca-pub-6025240004467345",
-          enable_page_level_ads: true
-     });
-</script>
+
--- resources/views/partials/_headblog.blade.php
+++ resources/views/partials/_headblog.blade.php
@@ -1,24 +1,28 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE-edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="apple-touch-icon" sizes="180x180" href="https://wknown/images/favicons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="https://wknown/images/favicons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="https://wknown/images/favicons/favicon-16x16.png">
-<link rel="manifest" href="https://wknown/images/favicons/manifest.json">
+<link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
 <meta name="theme-color" content="#ffffff">
+<meta name="msvalidate.01" content="BAFEFCC2D225DD9706515B2162FF1D71" />
+<meta name="p:domain_verify" content="79f1949805c90e9d8e15f318af06c4f9"/>
 <title>@yield('title')</title>
 <!-- Meta Keys -->
 <meta name="description" content="@yield('description')">
 <meta name="keywords" content="@yield('keywords')">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta property="og:title" content="@yield('title')" />
+<meta property="og:description" content="@yield('description')" />
 
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({
-          google_ad_client: "ca-pub-6025240004467345",
-          enable_page_level_ads: true
-     });
-</script>
+@yield('preload')
+<link href="{!! asset('css/bootstrap.min.css') !!}" media="all" rel="stylesheet" type="text/css" />
+<link href="{!! asset('css/now-ui-kit.css') !!}" media="all" rel="stylesheet" type="text/css" />
+<link href="{!! asset('css/aos.css') !!}" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+<script charset="UTF-8" src="//cdn.sendpulse.com/js/push/5e58d19181d74ca35a19fe08a4065c4d_1.js" async></script>
+
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109464444-1"></script>
 <script>
@@ -29,27 +33,23 @@
 	gtag('config', 'AW-823555244');
 </script>
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-52K9TDC');</script>
-<!-- End Google Tag Manager -->
-
-<style>
-  body {
-    background-color: #f2f2f2 !important;
-  }
-</style>
-<link rel="stylesheet prefetch" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-<link href="{!! asset('css/bootstrap.css') !!}" media="all" rel="stylesheet" type="text/css" />
-<link href="{!! asset('css/main.css') !!}" media="all" rel="stylesheet" type="text/css" />
-<link href="{!! asset('css/font-awesome.css') !!}" media="all" rel="stylesheet" type="text/css" />
-<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css">
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '279834189113087');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=279834189113087&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
 
 <!-- Hotjar Tracking Code for https://wknown.com -->
 <script>
@@ -62,8 +62,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
-
-<script src='https://www.google.com/recaptcha/api.js'></script>
+@yield('extrahead')
 <script type="text/javascript" src="https://widget.clutch.co/static/js/widget.js"></script>
 <script async src="https://cdn.ampproject.org/v0.js"></script>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
